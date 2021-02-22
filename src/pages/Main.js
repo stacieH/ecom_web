ca50e0971d0d98518b5fcfc5b793086c84f3b960
stacieH <stacie.harlene@gmail.com>
2021-02-22 16:49:59 +0800

    Main Page: call getFood using fetch with async & await

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,35 +3,35 @@ import React,{Fragment, useEffect, useState} from 'react'
 import Card from '../components/Card'
 import LoadingCard from '../components/LoadingCard'
 
-// import * as API from '../utils/API'
+import * as API from '../utils/API'
 
 import '../styles/Card.css'
 
 function Main () {
-    const [foods] = useState([]) 
-    const [loading] = useState(true) 
+    const [foods, setFoods] = useState([]) 
+    const [loading, setLoading] = useState(true) 
 
     useEffect(()=>{
-        // async function getFoodie(){
-        //     try{
-        //         const res = await API.getFood('seafood')
-        //         const {hits} = res
-        //         const foods = await hits.map(d=>{
-        //             const { recipe } = d
-        //             if(recipe){
-        //                 const {image, label, source} = recipe
-        //                 return {image, label, source}
-        //             }
-        //             return d
-        //         })
-        //         await setFoods(foods)
-        //         await setLoading(false)
-        //     }
-        //     catch(err){
-        //         setLoading(false)
-        //     }
-        // }
-        // getFoodie()
+        async function getFoodie(){
+            try{
+                const res = await API.getFoods('seafood')
+                const {hits} = res
+                const foods = await hits.map(d=>{
+                    const { recipe } = d
+                    if(recipe){
+                        const {image, label, source} = recipe
+                        return {image, label, source}
+                    }
+                    return d
+                })
+                await setFoods(foods)
+                await setLoading(false)
+            }
+            catch(err){
+                setLoading(false)
+            }
+        }
+        getFoodie()
     },[])
     
     if(!loading && foods.length===0){
--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ const api_url = 'https://api.edamam.com'
 const app_id = "7b7a1a76"
 const app_key = "f04c1976b0dcdf3c5f889aeea11f7e35"
 
-export const getFood = (query='') => {
+export const getFoods = (query='') => {
     return fetch(`${api_url}/search?q=${query}&app_id=${app_id}&app_key=${app_key}`,{
         method: 'GET',
         // headers:{
