f70ecb730d6d6d1e4956a60b626e63504205dab0
stacieH <stacie.harlene@gmail.com>
2021-03-15 16:14:21 +0800

    React Router: NavLink style
    color black -> brand name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import '../styles/Header.css';
+
 function Header() {
   return (
     <header>
       <h1>
-        <NavLink to="/">Food</NavLink>
+        <NavLink to="/" activeClassName="brandname">
+          Food
+        </NavLink>
       </h1>
     </header>
   );
