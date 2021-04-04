8f724c42bf85505a82815e090fdc1f04d2c70c80
stacieH <stacie.harlene@gmail.com>
2021-04-04 17:14:03 +0800

    Webpack: remove file-loader and use native

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports={
             },
             {
                 test:/\.(jpe?g|svg|gif|png)$/,
-                use:"file-loader"
+                type:"asset"
             }
         ],
     },
