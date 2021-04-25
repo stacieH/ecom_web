36b6f204c03498f9102d5d3f575ff01ba1b4784d
stacieH <stacie.harlene@gmail.com>
2021-04-25 17:17:27 +0800

    Redux: add thunk as middleware and remove logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
+import thunkMiddleware from 'redux-thunk';
 
 import { persistedReducer } from './persistor';
 
-const logger = () => (next) => (action) => {
-  let result = next(action);
-  return result;
-};
+// const logger = () => (next) => (action) => {
+//   let result = next(action);
+//   return result;
+// };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(
+const store = createStore(
   persistedReducer,
-  composeEnhancers(applyMiddleware(logger)),
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
 );
 
-export const persistor = persistStore(store);
+// store.subscribe(() => console.log(store.getState()));
+
+const persistor = persistStore(store);
+
+export { store, persistor };
